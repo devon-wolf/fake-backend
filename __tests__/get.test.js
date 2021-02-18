be48c4146735313099a0055810230cc51bedd375
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-18 14:35:32 -0800

    Simplify test to see if I am testing correctly

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -3,9 +3,9 @@ const supertest = require('supertest');
 const request = supertest(app);
 const { yarnInMyCloset } = require('../data.js');
 
-it('gets the yarn endpoint', async done => {
-	const myData = yarnInMyCloset;
-	const response = await request.get('/yarn');
+it('gets the home endpoint', async done => {
+	const myData = { greeting: 'This is the home endpoint. Welcome.' };
+	const response = await request.get('/');
   
 	expect(response.status).toBe(200)
 	expect(response.body.message).toEqual(myData)
